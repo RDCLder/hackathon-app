4dd0b0f680e10924b498bfb10edface82cbdf955
LannyWang009 <09legend.wang@gmail.com>
2019-04-13 18:26:14 -0500

    to save on styling CD

--- src/components/partials/AddCard.js
+++ src/components/partials/AddCard.js
@@ -1,75 +1,75 @@
-import React from "react";
-import { Container, Row, Col, Modal, Button, Alert } from "react-bootstrap";
+import React from 'react'
+import { Container, Row, Col, Modal, Button, Alert } from 'react-bootstrap'
 
 class AddCard extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
       show: false,
-      word: "",
-      text: "",
+      word: '',
+      text: '',
       alertMessage: null,
       alertShow: false
-    };
+    }
   }
 
-  changeWord(e) {
-    this.setState({ word: e.target.value });
+  changeWord (e) {
+    this.setState({ word: e.target.value })
   }
 
-  changeText(e) {
-    this.setState({ text: e.target.value });
+  changeText (e) {
+    this.setState({ text: e.target.value })
   }
 
-	handleShow() {
-		this.setState({ show: true })
-	}
+  handleShow () {
+    this.setState({ show: true })
+  }
 
-	handleClose() {
-		this.setState({ show: false })
-	}
+  handleClose () {
+    this.setState({ show: false })
+  }
 
-  addCard() {
-    const words = this.props.deck.map(card => card.word);
+  handleSubmit () {
+    const words = this.props.deck.map(card => card.word)
 
     // Error checks for empty word/text, long word/text, or existing word
-    if (this.state.word === "") {
+    if (this.state.word === '') {
       this.setState({
         alertMessage: "The word can't be blank!",
         alertShow: true
-      });
-    } else if (this.state.text === "") {
+      })
+    } else if (this.state.text === '') {
       this.setState({
         alertMessage: "The text can't be blank!",
         alertShow: true
-      });
+      })
     } else if (this.state.word.length > 40) {
       this.setState({
-        alertMessage: "The word cannot exceed 40 characters!",
+        alertMessage: 'The word cannot exceed 40 characters!',
         alertShow: true
-      });
+      })
     } else if (this.state.word.length > 200) {
       this.setState({
-        alertMessage: "The text cannot exceed 200 characters!",
+        alertMessage: 'The text cannot exceed 200 characters!',
         alertShow: true
-      });
+      })
     } else if (words.includes(this.state.word)) {
       this.setState({
         alertMessage: `${this.state.word} has already been used!`
-      });
-      this.setState({ alertShow: true });
+      })
+      this.setState({ alertShow: true })
     } else {
-			this.props.updateDeck({
-				word: this.state.word,
-				text: this.state.text
-			});
+      this.props.update({
+        word: this.state.word,
+        text: this.state.text
+      })
       this.setState({
         show: false,
-				alertShow: false,
-				word: "",
-				text: ""
-      });
+        alertShow: false,
+        word: '',
+        text: ''
+      })
     }
 
     // Closes error alert after 2 seconds
@@ -77,46 +77,46 @@ class AddCard extends React.Component {
       setTimeout(() => {
         this.setState({
           alertShow: false,
-          alertMessage: ""
-        });
-      }, 2000);
+          alertMessage: ''
+        })
+      }, 2000)
     }
   }
 
-  render() {
+  render () {
     return (
       <React.Fragment>
 
         <Button
-					variant="primary"
+          variant='primary'
           onClick={() => this.handleShow()}
         >
-          <i className="fas fa-plus" />
+          <i className='fas fa-plus' />
         </Button>
 
         <Modal
           show={this.state.show}
           onHide={this.handleClose}
           centered
-          size="lg"
+          size='lg'
         >
           <Modal.Header>
             <Modal.Title>Add Card</Modal.Title>
             <i
-              className="fas fa-times modalDismiss"
+              className='fas fa-times modalDismiss'
               onClick={() => this.handleClose()}
             />
           </Modal.Header>
           <Modal.Body>
-            <Container id="AddCardModalContainer">
+            <Container id='AddCardModalContainer'>
               <Row>
                 <h5>Word</h5>
               </Row>
-              <Row className="mb-4">
+              <Row className='mb-4'>
                 <textarea
-                  type="text"
-                  placeholder="Word goes here"
-                  rows="1"
+                  type='text'
+                  placeholder='Word goes here'
+                  rows='1'
                   onChange={e => this.changeWord(e)}
                 />
               </Row>
@@ -125,33 +125,33 @@ class AddCard extends React.Component {
               </Row>
               <Row>
                 <textarea
-                  type="text"
-                  placeholder="Text goes here"
-                  rows="5"
+                  type='text'
+                  placeholder='Text goes here'
+                  rows='5'
                   onChange={e => this.changeText(e)}
                 />
               </Row>
             </Container>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="primary" onClick={() => this.handleSubmit()}>
+            <Button variant='primary' onClick={() => this.handleSubmit()}>
               Submit
             </Button>
-            <Button variant="secondary" onClick={() => this.handleClose()}>
+            <Button variant='secondary' onClick={() => this.handleClose()}>
               Close
             </Button>
           </Modal.Footer>
         </Modal>
 
         <Alert
-          variant="danger"
+          variant='danger'
           show={this.state.alertShow}
-          className="alert"
+          className='alert'
         >
           <Alert.Heading>
             Error
             <i
-              className="fas fa-times alertDismiss"
+              className='fas fa-times alertDismiss'
               onClick={() => this.setState({ alertShow: false })}
             />
           </Alert.Heading>
@@ -159,8 +159,8 @@ class AddCard extends React.Component {
         </Alert>
 
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default AddCard;
+export default AddCard
--- src/components/views/CreateDeck.js
+++ src/components/views/CreateDeck.js
@@ -1,63 +1,63 @@
-import React from "react";
-import { Container, Row, Col, Card, Button, Alert } from "react-bootstrap";
-import AddCard from "../partials/AddCard";
+import React from 'react'
+import { Container, Row, Col, Card, Button, Alert } from 'react-bootstrap'
+import AddCard from '../partials/AddCard'
 
 class CreateDeck extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      deckName: "",
+      deckName: '',
       deck: [],
       alertMessage: null,
       alertShow: false
-    };
+    }
   }
 
-  changeDeckName(e) {
+  changeDeckName (e) {
     this.setState({
       deckName: e.target.value
-    });
+    })
   }
 
-  updateDeck(card) {
+  updateDeck (card) {
     this.setState({
       deck: this.state.deck.concat(card)
-    });
+    })
   }
 
-  deleteFromDeck(word) {
-    let newDeck = this.state.deck.slice();
-    delete newDeck[word];
-    this.setState({ deck: newDeck });
+  deleteFromDeck (word) {
+    let newDeck = this.state.deck.slice()
+    delete newDeck[word]
+    this.setState({ deck: newDeck })
   }
 
-  addDeck() {
-		// const deckNames = Object.keys(allDecks);
-		const deckNames = [];
+  addDeck () {
+    // const deckNames = Object.keys(allDecks);
+    const deckNames = []
 
     // Error checks for empty name, name over 40 characters, or existing name
-    if (this.state.deckName === "") {
+    if (this.state.deckName === '') {
       this.setState({
         alertMessage: "The deck name can't be empty!",
         alertShow: true
-      });
+      })
     } else if (this.state.deckName.length > 40) {
       this.setState({
         alertMessage: "The deck name can't exceed 40 characters!",
         alertShow: true
-      });
+      })
     } else if (deckNames.includes(this.state.deckName)) {
       this.setState({
-        alertMessage: "The deck name is already taken!",
+        alertMessage: 'The deck name is already taken!',
         alertShow: true
-      });
+      })
     } else {
       // Update database
       this.setState({
-        deckName: "",
+        deckName: '',
         alertShow: false
-      });
+      })
     }
 
     // Closes error alert after 2 seconds
@@ -65,72 +65,92 @@ class CreateDeck extends React.Component {
       setTimeout(() => {
         this.setState({
           alertShow: false,
-          alertMessage: ""
-        });
-      }, 2000);
+          alertMessage: ''
+        })
+      }, 2000)
     }
   }
 
-  render() {
+  render () {
     // const deck = this.state.deck.map(card => (
-		// 	<Card id={card.word} 
-		// 		word={card.word} 
-		// 		text={card.text}
-		// 		delete={word => this.deleteFromDeck(word)}
-		// 	/>
+    // 	<Card id={card.word}
+    // 		word={card.word}
+    // 		text={card.text}
+    // 		delete={word => this.deleteFromDeck(word)}
+    // 	/>
 
-			const deck = this.state.deck.map(card => (
-				<Card key={card.word}>
-					<Card.Body>
-							<Card.Title>
-									{card.word}
-									<i className="fas fa-times cardButton"
-											onClick={() => this.deleteFromDeck(card.word)}
-									/>
-									<i className="fas fa-pencil-alt cardButton"
-									/>
-							</Card.Title>
-							<Card.Text className="cardText">
-									{card.text}
-							</Card.Text>
-					</Card.Body>
-			</Card>
-		));
+    const deck = this.state.deck.map(card => (
+      <Card key={card.word}>
+        <Card.Body>
+          <Card.Title>
+            {card.word}
+            <i className='fas fa-times cardButton'
+              onClick={() => this.deleteFromDeck(card.word)}
+            />
+            <i className='fas fa-pencil-alt cardButton'
+            />
+          </Card.Title>
+          <Card.Text className='cardText'>
+            {card.text}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    ))
+
+    // ======= this is css style object=============
+    const colorscheme = ['#cca0af',
+      '#d9eeb6',
+      '#c6c0f7',
+      '#94dfff',
+      '#ffdcf6']
+    const cd = {
+
+      title: {
+        textAlign: 'left',
+        color: colorscheme[1],
+        fontWeight: '300'
+      },
+      plus: {
+        backgroundColor: colorscheme[1]
+      }
+    }
 
     return (
-      <Container fluid="true">
-
-        <Row>
-          <h3>Create New Deck</h3>
-        </Row>
-
-        <Row>
-          <Col>
-            <input></input>
-          </Col>
-          <Col xs={2}>
-            <Button variant="primary" onClick={() => this.addDeck()}>
+      <Container fluid='true'>
+        <Container>
+          <Row>
+            <Col xs={12} md={11}>
+              <h2 style={cd.title}>Create a new deck:</h2>
+            </Col>
+            <Col xs={4} md={1} >
+              <AddCard style={cd.plus}
+                deck={this.state.deck}
+                update={card => this.updateDeck(card)}
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={8}>space holder for input</Col>
+            <Col md={4}>whatever</Col>
+          </Row>
+          <Row>
+            <Button variant={colorscheme[2]} onClick={() => this.addDeck()}>
               SAVE DECK
             </Button>
-          </Col>
-        </Row>
+          </Row>
+        </Container>
 
         <Row>{deck}</Row>
 
-        <AddCard
-          deck={this.state.deck}
-          update={card => this.updateDeck(card)}
-        />
-
         <Alert
-          variant="danger"
+          variant='danger'
           show={this.state.alertShow}
-          className="alert"
+          className='alert'
         >
           <Alert.Heading>
             Error
             <i
-              className="fas fa-times alertDismiss"
+              className='fas fa-times alertDismiss'
               onClick={() => this.setState({ alertShow: false })}
             />
           </Alert.Heading>
@@ -138,7 +158,7 @@ class CreateDeck extends React.Component {
         </Alert>
 
       </Container>
-    );
+    )
   }
 }
 
@@ -146,4 +166,4 @@ class CreateDeck extends React.Component {
 // <i className="fas fa-trash" />
 // <i className="fas fa-times" />
 
-export default CreateDeck;
+export default CreateDeck
